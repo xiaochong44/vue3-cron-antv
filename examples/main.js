e695d27b09597ba4422a7550dec33fdf9e0978d4
“feng <277324181@qq.com>
2021-09-29 10:30:51 +0800

    修改版本

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import VueCron from '../packages';
+import VueCron from '../';
 import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue';
 createApp(App).use(VueCron).use(Antd).mount('#app');
